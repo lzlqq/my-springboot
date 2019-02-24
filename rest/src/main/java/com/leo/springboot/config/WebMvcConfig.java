8a118f400450e9dede5b89e7dc8e93bb53ef6dc4
lzlqq <390650599@qq.com>
2019-02-24 23:27:27 +0800

    rest

---
+++ rest/src/main/java/com/leo/springboot/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.leo.springboot.config;
+
+import com.leo.springboot.http.message.PropertiesPersonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * Web MVC 配置
+ *
+ * @author mercyblitz
+ * @date 2017-10-14
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void extendMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+
+        converters.add(new PropertiesPersonHttpMessageConverter());
+    }
+
+}
