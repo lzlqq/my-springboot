00726116be9741302242792dba14542995b6fa0e
lzlqq <390650599@qq.com>
2019-07-13 23:36:06 +0800

    拦截器配置

--- my-springsecurity/security-demo/src/main/java/com/leo/web/config/WebConfig.java
+++ my-springsecurity/security-demo/src/main/java/com/leo/web/config/WebConfig.java
@@ -3,14 +3,27 @@ package com.leo.web.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.leo.web.filter.TimeFilter;
+import com.leo.web.interceptor.TimeInterceptor;
 
 @Configuration
-public class WebConfig {
+public class WebConfig  extends WebMvcConfigurerAdapter {
+	
+	@Autowired
+	private TimeInterceptor timeInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(timeInterceptor);
+	}
+	
 	@Bean
 	public FilterRegistrationBean timeFilter() {
 		
