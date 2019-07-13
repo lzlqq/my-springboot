f263063411a4b44e840b7263553c33f8cf5aae38
lzlqq <390650599@qq.com>
2019-07-14 00:19:06 +0800

    文件上传，下载

--- my-springsecurity/security-demo/src/test/java/com/leo/web/controller/UserControllerTest.java
+++ my-springsecurity/security-demo/src/test/java/com/leo/web/controller/UserControllerTest.java
@@ -4,6 +4,7 @@
 package com.leo.web.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -20,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -133,6 +135,15 @@ public class UserControllerTest {
 		mockMvc.perform(delete("/user/1")
 				.contentType(MediaType.APPLICATION_JSON_UTF8))
 				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void whenUploadSuccess() throws Exception {
+		String result = mockMvc.perform(fileUpload("/file")
+				.file(new MockMultipartFile("file", "test.txt", "multipart/form-data", "hello upload".getBytes("UTF-8"))))
+				.andExpect(status().isOk())
+				.andReturn().getResponse().getContentAsString();
+		System.out.println(result);
 	}
 	
 }
