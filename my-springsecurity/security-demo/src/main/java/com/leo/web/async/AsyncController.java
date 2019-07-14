7adf3f467bfb22a48ab61041f102324d6c70471f
lzlqq <390650599@qq.com>
2019-07-14 14:45:28 +0800

    多线程提高REST性能 Runnable DeferredResult 异步拦截器配置

--- my-springsecurity/security-demo/src/main/java/com/leo/web/async/AsyncController.java
+++ my-springsecurity/security-demo/src/main/java/com/leo/web/async/AsyncController.java
@@ -3,9 +3,7 @@
  */
 package com.leo.web.async;
 
-import java.util.concurrent.Callable;
-
-import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,40 +17,38 @@ import org.springframework.web.context.request.async.DeferredResult;
  */
 @RestController
 public class AsyncController {
-	
-//	@Autowired
-//	private MockQueue mockQueue;
-//	
-//	@Autowired
-//	private DeferredResultHolder deferredResultHolder;
-	
+
+	@Autowired
+	private MockQueue mockQueue;
+
+	@Autowired
+	private DeferredResultHolder deferredResultHolder;
+
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+
 	@RequestMapping("/order")
-	public Callable<String> order() throws Exception {
-//		public DeferredResult<String> order() throws Exception {
+	// public Callable<String> order() throws Exception {
+	public DeferredResult<String> order() throws Exception {
 		logger.info("主线程开始");
-		
-//		String orderNumber = RandomStringUtils.randomNumeric(8);
-//		mockQueue.setPlaceOrder(orderNumber);
-//		
-//		DeferredResult<String> result = new DeferredResult<>();
-//		deferredResultHolder.getMap().put(orderNumber, result);
-		
-		
-		
-		Callable<String> result = new Callable<String>() {
-			@Override
-			public String call() throws Exception {
-				logger.info("副线程开始");
-				Thread.sleep(1000);
-				logger.info("副线程返回");
-				return "success";
-			}
-		};
-		
+
+		String orderNumber = RandomStringUtils.randomNumeric(8);
+		mockQueue.setPlaceOrder(orderNumber);
+
+		DeferredResult<String> result = new DeferredResult<>();
+		deferredResultHolder.getMap().put(orderNumber, result);
+
+		// Callable<String> result = new Callable<String>() {
+		// @Override
+		// public String call() throws Exception {
+		// logger.info("副线程开始");
+		// Thread.sleep(1000);
+		// logger.info("副线程返回");
+		// return "success";
+		// }
+		// };
+
 		logger.info("主线程结束");
-		
+
 		return result;
 	}
 
