2f742446d19f4ba68d54d495d2d50440e2c15427
lzlqq <390650599@qq.com>
2019-07-14 17:42:34 +0800

    处理用户校验逻辑UserDetails 处理密码加密解密PasswordEncoder

--- my-springsecurity/security-browser/src/main/java/com/leo/security/browser/BrowserSecurityConfig.java
+++ my-springsecurity/security-browser/src/main/java/com/leo/security/browser/BrowserSecurityConfig.java
@@ -3,9 +3,12 @@
  */
 package com.leo.security.browser;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author zhailiang
@@ -89,10 +92,10 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 //		
 //	}
 //
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 //	
 //	@Bean
 //	public PersistentTokenRepository persistentTokenRepository() {
--- my-springsecurity/security-browser/src/main/java/com/leo/security/browser/MyUserDetailsService.java
+++ my-springsecurity/security-browser/src/main/java/com/leo/security/browser/MyUserDetailsService.java
@@ -2,17 +2,22 @@ package com.leo.security.browser;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyUserDetailsService implements UserDetailsService{
 	
 	private Logger logger = LoggerFactory.getLogger(MyUserDetailsService.class);
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -20,7 +25,9 @@ public class MyUserDetailsService implements UserDetailsService{
 		// 根据用户名查找用户信息
 		// 根据查找到的用户判断用户是否被冻结
 		// return new User(username, "123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
-		return new User(username, "123456",true,true,true,true, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+		String password = passwordEncoder.encode("123456");
+		logger.info("数据库密码是："+password);
+		return new User(username,password,true,true,true,true, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
 	}
 
 }
