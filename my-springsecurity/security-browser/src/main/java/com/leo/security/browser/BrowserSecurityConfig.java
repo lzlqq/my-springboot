7161c3448af190ed4e09908ea6a76476dc7a40a7
lzlqq <390650599@qq.com>
2019-07-21 00:00:41 +0800

    短信验证码验证&登录  * 1.短信验证码验证过滤器，验证短信验证码是否正确  * 2.短信登录认证过滤器，根据手机号获取用户信息  * 3.自定义短信登录Token  * 4.自定义短信登录Provider  * 5.根据流程图，配置组装这些类

--- my-springsecurity/security-browser/src/main/java/com/leo/security/browser/BrowserSecurityConfig.java
+++ my-springsecurity/security-browser/src/main/java/com/leo/security/browser/BrowserSecurityConfig.java
@@ -19,16 +19,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import com.leo.security.core.authentication.AbstractChannelSecurityConfig;
+import com.leo.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.leo.security.core.properties.SecurityConstants;
 import com.leo.security.core.properties.SecurityProperties;
 import com.leo.security.core.validate.code.ValidateCodeFilter;
+import com.leo.security.core.validate.code.ValidateCodeSecurityConfig;
 
 /**
  * @author zhailiang
  *
  */
 @Configuration
-//public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
-public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
+//public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private SecurityProperties securityProperties;
 	
@@ -37,12 +41,12 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
-//	
-//	@Autowired
-//	private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
-//	
-//	@Autowired
-//	private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+	
+	@Autowired
+	private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+	
+	@Autowired
+	private ValidateCodeSecurityConfig validateCodeSecurityConfig;
 //	
 //	@Autowired
 //	private SpringSocialConfigurer imoocSocialSecurityConfig;
@@ -53,58 +57,58 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 //	@Autowired
 //	private InvalidSessionStrategy invalidSessionStrategy;
 	
-	@Autowired
-	private ValidateCodeFilter validateCodeFilter;
+//	@Autowired
+//	private ValidateCodeFilter validateCodeFilter;
 	
-	@Autowired
-	private AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
-	
-	@Autowired
-	private AuthenticationFailureHandler imoocAuthenticationFailureHandler;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
-			.formLogin()
-			//http.httpBasic()
-				//.loginPage("/imooc-signIn.html")  //登录页面
-				.loginPage("/authentication/require")  //自定义认证控制器接口
-				.loginProcessingUrl("/authentication/form") //申明使用UsernamePasswordxx过滤器验证登录
-				.successHandler(imoocAuthenticationSuccessHandler) //自定义成功认证之后处理器
-				.failureHandler(imoocAuthenticationFailureHandler)//自定义失败认证之后处理器
-				.and()
-			.rememberMe()
-				.tokenRepository(persistentTokenRepository())
-				.tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
-				.userDetailsService(userDetailsService)
-				.and()			
-			.authorizeRequests()
-			//.antMatchers("/authentication/require").permitAll()
-			.antMatchers("/authentication/require",
-					securityProperties.getBrowser().getLoginPage(),
-					"/code/*").permitAll()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.csrf().disable();
-	}
+//	@Autowired
+//	private AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
 //	
+//	@Autowired
+//	private AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+	
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
-//		
-//		applyPasswordAuthenticationConfig(http);
-//		
-//		http.apply(validateCodeSecurityConfig)
-//				.and()
-//			.apply(smsCodeAuthenticationSecurityConfig)
-//				.and()
-//			.apply(imoocSocialSecurityConfig)
+//		http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+//			.formLogin()
+//			//http.httpBasic()
+//				//.loginPage("/imooc-signIn.html")  //登录页面
+//				.loginPage("/authentication/require")  //自定义认证控制器接口,SpringSecurity判断某个接口需要认证登录，这个接口负责给前端返回页面或者JSON
+//				.loginProcessingUrl("/authentication/form") //上面那个配置如果返回时页面的化，页面上面的登录表单接口是这个，SpringSecurity中验证这个接口使用UsernamePasswordxx过滤器验证登录
+//				.successHandler(imoocAuthenticationSuccessHandler) //自定义成功认证之后处理器
+//				.failureHandler(imoocAuthenticationFailureHandler)//自定义失败认证之后处理器
 //				.and()
 //			.rememberMe()
 //				.tokenRepository(persistentTokenRepository())
 //				.tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
 //				.userDetailsService(userDetailsService)
+//				.and()			
+//			.authorizeRequests()
+//			//.antMatchers("/authentication/require").permitAll()
+//			.antMatchers("/authentication/require",
+//					securityProperties.getBrowser().getLoginPage(),
+//					"/code/*").permitAll()
+//			.anyRequest()
+//			.authenticated()
+//			.and()
+//			.csrf().disable();
+//	}
+//	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		applyPasswordAuthenticationConfig(http);
+		
+		http.apply(validateCodeSecurityConfig)
+				.and()
+			.apply(smsCodeAuthenticationSecurityConfig)
+				.and()
+//			.apply(imoocSocialSecurityConfig)
 //				.and()
+			.rememberMe()
+				.tokenRepository(persistentTokenRepository())
+				.tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+				.userDetailsService(userDetailsService)
+				.and()
 //			.sessionManagement()
 //				.invalidSessionStrategy(invalidSessionStrategy)
 //				.maximumSessions(securityProperties.getBrowser().getSession().getMaximumSessions())
@@ -112,24 +116,24 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 //				.expiredSessionStrategy(sessionInformationExpiredStrategy)
 //				.and()
 //				.and()
-//			.authorizeRequests()
-//				.antMatchers(
-//					SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
-//					SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
-//					securityProperties.getBrowser().getLoginPage(),
-//					SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
-//					securityProperties.getBrowser().getSignUpUrl(),
+			.authorizeRequests()
+				.antMatchers(
+					SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+					SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+					securityProperties.getBrowser().getLoginPage(),
+					SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
+					securityProperties.getBrowser().getSignUpUrl(),
 //					securityProperties.getBrowser().getSession().getSessionInvalidUrl()+".json",
 //					securityProperties.getBrowser().getSession().getSessionInvalidUrl()+".html",
-//					"/user/regist")
-//					.permitAll()
-//				.anyRequest()
-//				.authenticated()
-//				.and()
-//			.csrf().disable();
-//		
-//	}
-//
+					"/user/regist")
+					.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+			.csrf().disable();
+		
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
