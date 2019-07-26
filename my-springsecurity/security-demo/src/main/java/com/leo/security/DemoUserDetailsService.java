4890edd1b04b4da74235240b970272153ce8bd76
lzlqq <390650599@qq.com>
2019-07-26 15:28:57 +0800

    格式化代码

--- my-springsecurity/security-demo/src/main/java/com/leo/security/DemoUserDetailsService.java
+++ my-springsecurity/security-demo/src/main/java/com/leo/security/DemoUserDetailsService.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,39 +14,36 @@ import org.springframework.social.security.SocialUserDetailsService;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DemoUserDetailsService implements UserDetailsService, SocialUserDetailsService{
-	
-	private Logger logger = LoggerFactory.getLogger(DemoUserDetailsService.class);
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+public class DemoUserDetailsService implements UserDetailsService,SocialUserDetailsService{
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		logger.info("表单登录用户名："+username);
-		// 根据用户名查找用户信息
-		// 根据查找到的用户判断用户是否被冻结
-		// return new User(username, "123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
-//		String password = passwordEncoder.encode("123456");
-//		logger.info("数据库密码是："+password);
-//		return new User(username,password,true,true,true,true, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
-		return buildUser(username);
-	}
+    private Logger logger = LoggerFactory.getLogger(DemoUserDetailsService.class);
 
-	
-	@Override
-	public SocialUserDetails loadUserByUserId(String userId) throws UsernameNotFoundException {
-		logger.info("社交登录用户Id:" + userId);
-		return buildUser(userId);
-	}
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-	private SocialUserDetails buildUser(String userId) {
-		// 根据用户名查找用户信息
-		//根据查找到的用户信息判断用户是否被冻结
-		String password = passwordEncoder.encode("123456");
-		logger.info("数据库密码是:"+password);
-		return new SocialUser(userId, password,
-				true, true, true, true,
-				AuthorityUtils.commaSeparatedStringToAuthorityList("admin,ROLE_USER"));
-	}
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        logger.info("表单登录用户名：" + username);
+        // 根据用户名查找用户信息
+        // 根据查找到的用户判断用户是否被冻结
+        // return new User(username, "123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        // String password = passwordEncoder.encode("123456");
+        // logger.info("数据库密码是："+password);
+        // return new User(username,password,true,true,true,true, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        return buildUser(username);
+    }
+
+    @Override
+    public SocialUserDetails loadUserByUserId(String userId) throws UsernameNotFoundException{
+        logger.info("社交登录用户Id:" + userId);
+        return buildUser(userId);
+    }
+
+    private SocialUserDetails buildUser(String userId){
+        // 根据用户名查找用户信息
+        //根据查找到的用户信息判断用户是否被冻结
+        String password = passwordEncoder.encode("123456");
+        logger.info("数据库密码是:" + password);
+        return new SocialUser(userId, password, true, true, true, true, AuthorityUtils.commaSeparatedStringToAuthorityList("admin,ROLE_USER"));
+    }
 }
