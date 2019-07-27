37a626b4e6a67c0a878db8120091945b44e1cad8
lzlqq <390650599@qq.com>
2019-07-27 10:41:55 +0800

    03 - 自定义用户名密码登录&资源服务器配置

--- my-springsecurity/security-app/src/main/java/com/leo/security/app/ImoocResourceServerConfig.java
+++ my-springsecurity/security-app/src/main/java/com/leo/security/app/ImoocResourceServerConfig.java
@@ -3,43 +3,56 @@
  */
 package com.leo.security.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+import com.leo.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.leo.security.core.properties.SecurityConstants;
+import com.leo.security.core.properties.SecurityProperties;
 
 /**
- * 资源服务器配置
+ * app资源服务器配置
  * 
  * @author zhailiang
+ * 
+ * 认证服务器同时作为资源服务器，别人来访问资源的时候配置应该是怎么样的
  *
  */
 @Configuration
 @EnableResourceServer
-public class ImoocResourceServerConfig {
-//extends ResourceServerConfigurerAdapter {
-//	
-//	@Autowired
-//	protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
-//	
-//	@Autowired
-//	protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
-//	
-//	@Autowired
-//	private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+public class ImoocResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	@Autowired
+	protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
+	
+	@Autowired
+	protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+	
+	@Autowired
+	private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
 //	
 //	@Autowired
 //	private OpenIdAuthenticationSecurityConfig openIdAuthenticationSecurityConfig;
 //	
 //	@Autowired
 //	private ValidateCodeSecurityConfig validateCodeSecurityConfig;
-//	
-//	@Autowired
-//	private SpringSocialConfigurer imoocSocialSecurityConfig;
+	
+	@Autowired
+	private SpringSocialConfigurer imoocSocialSecurityConfig;
 //	
 //	@Autowired
 //	private AuthorizeConfigManager authorizeConfigManager;
 //	
 //	@Autowired
 //	private FormAuthenticationConfig formAuthenticationConfig;
+	@Autowired
+	private SecurityProperties securityProperties;
 //	
 //	@Override
 //	public void configure(HttpSecurity http) throws Exception {
@@ -58,5 +71,38 @@ public class ImoocResourceServerConfig {
 //		
 //		authorizeConfigManager.config(http.authorizeRequests());
 //	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.formLogin()
+		.loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+		.loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+		.successHandler(imoocAuthenticationSuccessHandler)
+		.failureHandler(imoocAuthenticationFailureHandler);
+		
+
+		http //.apply(validateCodeSecurityConfig)
+				//.and()
+			.apply(smsCodeAuthenticationSecurityConfig)
+				.and()
+			.apply(imoocSocialSecurityConfig)
+				.and()
+			.authorizeRequests()
+				.antMatchers(
+					SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+					SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+					securityProperties.getBrowser().getLoginPage(),
+					SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
+					securityProperties.getBrowser().getSignUpUrl(),
+					securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+					securityProperties.getBrowser().getSignOutUrl(),
+					"/user/regist"
+					)
+					.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+			.csrf().disable();
+	}
 	
 }
