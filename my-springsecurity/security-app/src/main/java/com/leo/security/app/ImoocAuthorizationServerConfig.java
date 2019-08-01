8addd5057d6b3069498ef34c6109284027026a41
lzlqq <390650599@qq.com>
2019-08-01 23:37:34 +0800

    自定义配置多个client

--- my-springsecurity/security-app/src/main/java/com/leo/security/app/ImoocAuthorizationServerConfig.java
+++ my-springsecurity/security-app/src/main/java/com/leo/security/app/ImoocAuthorizationServerConfig.java
@@ -3,14 +3,19 @@
  */
 package com.leo.security.app;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+import com.leo.security.core.properties.OAuth2ClientProperties;
+import com.leo.security.core.properties.SecurityProperties;
 
 /**
  * 认证服务器配置
@@ -21,8 +26,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @Configuration
 @EnableAuthorizationServer
 public class ImoocAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    // 此处要是集成了AuthorizationServerConfigurerAdapter，会报java.lang.UnsupportedOperationException: Cannot build client services (maybe use inMemory() or jdbc()).
-    // copy代码，一定要核对清楚，既然原来都没问题，新加copy了代码进来，而视频中没问题的，说明copy进来的时候，肯定是多了什么，或者少了什么
+// 此处要是集成了AuthorizationServerConfigurerAdapter，会报java.lang.UnsupportedOperationException: Cannot build client services (maybe use inMemory() or jdbc()).
+// copy代码，一定要核对清楚，既然原来都没问题，新加copy了代码进来，而视频中没问题的，说明copy进来的时候，肯定是多了什么，或者少了什么
 
 //
 //	/**
@@ -43,9 +48,9 @@ public class ImoocAuthorizationServerConfig extends AuthorizationServerConfigure
 //
 //	@Autowired(required = false)
 //	private TokenEnhancer jwtTokenEnhancer;
-//
-//	@Autowired
-//	private SecurityProperties securityProperties;
+
+	@Autowired
+	private SecurityProperties securityProperties;
 
 	/**
 	 * 认证及token配置
@@ -75,35 +80,35 @@ public class ImoocAuthorizationServerConfig extends AuthorizationServerConfigure
 //	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.tokenKeyAccess("permitAll()");
 //	}
-//
-//	/**
-//	 * 客户端配置
-//	 * 我们这里不会像qq或者微信那样允许其他应用注册，所以将客户端信息放在内存中就可以了
-//	 */
-//	@Override
-//	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//		// 客户端信息在配置文件中配置
-//		InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
-//		if (ArrayUtils.isNotEmpty(securityProperties.getOauth2().getClients())) {
-//			for (OAuth2ClientProperties client : securityProperties.getOauth2().getClients()) {
-//				builder.withClient(client.getClientId())
-//						.secret(client.getClientSecret())
-//						.authorizedGrantTypes("refresh_token", "authorization_code", "password") // 客户端支持的授权模式
-//						.accessTokenValiditySeconds(client.getAccessTokenValidateSeconds()) // 令牌的有效时间
-//						.refreshTokenValiditySeconds(2592000)
-//						.scopes("all"); // 令牌的权限有哪些，可以配置多个，这里配置了，客户端请求时就不用带scope参数了.如果客户端带了scope参数，则必须是配置的scope中的其中一个，否则会报错
-//			}
-//		}
-//	}
-	
+
+	/**
+	 * 客户端配置
+	 * 我们这里不会像qq或者微信那样允许其他应用注册，所以将客户端信息放在内存中就可以了
+	 */
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("imooc")
-		.secret("imoocsecret")
-		.authorizedGrantTypes("refresh_token", "authorization_code", "password") // 客户端支持的授权模式
-		.accessTokenValiditySeconds(7200) // 令牌的有效时间
-		.refreshTokenValiditySeconds(2592000)
-		.scopes("all","read","write");
+		// 客户端信息在配置文件中配置
+		InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
+		if (ArrayUtils.isNotEmpty(securityProperties.getOauth2().getClients())) {
+			for (OAuth2ClientProperties client : securityProperties.getOauth2().getClients()) {
+				builder.withClient(client.getClientId())
+						.secret(client.getClientSecret())
+						.authorizedGrantTypes("refresh_token", "authorization_code", "password") // 客户端支持的授权模式
+						.accessTokenValiditySeconds(client.getAccessTokenValidateSeconds()) // 令牌的有效时间
+						.refreshTokenValiditySeconds(2592000)
+						.scopes("all"); // 令牌的权限有哪些，可以配置多个，这里配置了，客户端请求时就不用带scope参数了.如果客户端带了scope参数，则必须是配置的scope中的其中一个，否则会报错
+			}
+		}
 	}
+	
+//	@Override
+//	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//		clients.inMemory().withClient("imooc")
+//		.secret("imoocsecret")
+//		.authorizedGrantTypes("refresh_token", "authorization_code", "password") // 客户端支持的授权模式
+//		.accessTokenValiditySeconds(7200) // 令牌的有效时间
+//		.refreshTokenValiditySeconds(2592000)
+//		.scopes("all","read","write");
+//	}
 
 }
--- my-springsecurity/security-core/src/main/java/com/leo/security/core/properties/SecurityProperties.java
+++ my-springsecurity/security-core/src/main/java/com/leo/security/core/properties/SecurityProperties.java
@@ -6,17 +6,27 @@ package com.leo.security.core.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * @author zhailiang
  *
  */
 @ConfigurationProperties(prefix = "imooc.security")
 public class SecurityProperties {
-	
+
+	/**
+	 * 浏览器环境配置
+	 */
 	private BrowserProperties browser = new BrowserProperties();
-	
+	/**
+	 * 验证码配置
+	 */
 	private ValidateCodeProperties code = new ValidateCodeProperties();
-	
+	/**
+	 * 社交登录配置
+	 */
 	private SocialProperties social = new SocialProperties();
+	/**
+	 * OAuth2认证服务器配置
+	 */
+	private OAuth2Properties oauth2 = new OAuth2Properties();
 
 	public BrowserProperties getBrowser() {
 		return browser;
@@ -41,5 +51,13 @@ public class SecurityProperties {
 	public void setSocial(SocialProperties social) {
 		this.social = social;
 	}
-}
 
+	public OAuth2Properties getOauth2() {
+		return oauth2;
+	}
+
+	public void setOauth2(OAuth2Properties oauth2) {
+		this.oauth2 = oauth2;
+	}
+
+}
