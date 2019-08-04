469e3688de9c25e685822d4a8a16f7d0ce48b83f
lzlqq <390650599@qq.com>
2019-08-04 21:27:19 +0800

    简单角色控制，控制RESTful方法级和模糊路径

--- my-springsecurity/security-browser/src/main/java/com/leo/security/browser/BrowserSecurityConfig.java
+++ my-springsecurity/security-browser/src/main/java/com/leo/security/browser/BrowserSecurityConfig.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -110,7 +111,8 @@ public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
 //					,"/session/invalid"
 					)
 					.permitAll()
-				.antMatchers("/user").hasRole("ADMIN")
+//				.antMatchers("/user").hasRole("ADMIN")
+				.antMatchers(HttpMethod.GET,"/user/*").hasRole("ADMIN")
 				.anyRequest()
 				.authenticated()
 				.and()
