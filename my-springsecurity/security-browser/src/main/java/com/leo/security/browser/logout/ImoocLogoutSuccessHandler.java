7dcd46fd287e8ad5a2e254b106216df26fa49f46
lzlqq <390650599@qq.com>
2019-08-06 13:58:15 +0800

    browser logout 成功处理器

--- my-springsecurity/security-browser/src/main/java/com/leo/security/browser/logout/ImoocLogoutSuccessHandler.java
+++ my-springsecurity/security-browser/src/main/java/com/leo/security/browser/logout/ImoocLogoutSuccessHandler.java
@@ -13,35 +13,36 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.leo.security.core.properties.SecurityProperties;
 import com.leo.security.core.support.SimpleResponse;
 
+/**
+ * 默认的退出成功处理器，如果设置了imooc.security.browser.signOutUrl，则跳到配置的地址上，
+ * 如果没配置，则返回json格式的响应。
+ * 
+ *
+ */
 public class ImoocLogoutSuccessHandler implements LogoutSuccessHandler{
 
-	private final Logger logger = LoggerFactory.getLogger(getClass()); 
-	
-	private SecurityProperties securityProperties;
-	
-	private  ObjectMapper  objectMapper = new ObjectMapper();
-	public ImoocLogoutSuccessHandler(SecurityProperties securityProperties) {
-		this.securityProperties=securityProperties;
-	}
-	
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	
-	@Override
-	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-			throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		logger.info("退出成功");
-		
-		String signOutUrl=securityProperties.getBrowser().getSignOutUrl();
-		if(StringUtils.isBlank(signOutUrl)) {
-			response.setContentType("application/json;charset=UTF-8");
-			response.getWriter().write(objectMapper.writeValueAsString(new SimpleResponse("退出成功")));
-		}else {
-			response.sendRedirect(signOutUrl);
-		}
-	}
+    private String signOutSuccessUrl;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public ImoocLogoutSuccessHandler(String signOutSuccessUrl){
+        this.signOutSuccessUrl = signOutSuccessUrl;
+    }
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request,HttpServletResponse response,Authentication authentication) throws IOException,ServletException{
+        logger.info("退出成功");
+
+        if (StringUtils.isBlank(signOutSuccessUrl)){
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(objectMapper.writeValueAsString(new SimpleResponse("退出成功")));
+        }else{
+            response.sendRedirect(signOutSuccessUrl);
+        }
+    }
 
 }
