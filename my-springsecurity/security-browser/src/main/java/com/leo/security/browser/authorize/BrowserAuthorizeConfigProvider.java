8ca8acf0aa8176e06de3fefd7a6854e3177ee720
lzlqq <390650599@qq.com>
2019-08-06 13:57:45 +0800

    browser authorize包授权优化

---
+++ my-springsecurity/security-browser/src/main/java/com/leo/security/browser/authorize/BrowserAuthorizeConfigProvider.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.leo.security.browser.authorize;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.stereotype.Component;
+
+import com.leo.security.core.authorize.AuthorizeConfigProvider;
+
+/**
+ * 浏览器环境默认的授权配置，对常见的静态资源，如js,css，图片等不验证身份
+ * 
+ *
+ */
+@Component
+@Order(Integer.MIN_VALUE)
+public class BrowserAuthorizeConfigProvider implements AuthorizeConfigProvider{
+
+    @Override
+    public boolean config(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry config){
+        config.antMatchers(HttpMethod.GET, "/**/*.js", "/**/*.css", "/**/*.jpg", "/**/*.png", "/**/*.gif").permitAll();
+        return false;
+    }
+
+}
